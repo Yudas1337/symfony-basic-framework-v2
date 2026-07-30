3a58ae101432cad56517bd91b3e84c9615df8d49
Yudas Malabi <49679669+Yudas1337@users.noreply.github.com>
2026-07-30 23:11:42 +0700

    testing basic symfony framework

---
+++ src/app.php
@@ -0,0 +1,22 @@
+<?php
+
+use Symfony\Component\Routing;
+
+function is_leap_year(?int $year = null): bool
+{
+    if (null === $year) {
+        $year = (int)date('Y');
+    }
+
+    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
+
+
+$routes = new Routing\RouteCollection();
+
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
+    'year' => null,
+    '_controller' => 'Calendar\Controller\LeapYearController::index',
+]));
+
+return $routes;
