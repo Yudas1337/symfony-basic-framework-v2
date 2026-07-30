240bcc06e28f87e6681b13916e523fb3ab83fa6c
Yudas Malabi <49679669+Yudas1337@users.noreply.github.com>
2026-07-31 02:01:14 +0700

    add route fallback handling, testing for event dispatcher symfony

--- src/Core/Framework.php
+++ src/Core/Framework.php
@@ -2,6 +2,7 @@
 
 namespace Core;
 
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
@@ -12,6 +13,7 @@ use Symfony\Component\Routing\Matcher\UrlMatcher;
 class Framework
 {
     public function __construct(
+        private EventDispatcher $dispatcher,
         private UrlMatcher $matcher,
         private ControllerResolver $controllerResolver,
         private ArgumentResolver $argumentResolver,
@@ -27,11 +29,13 @@ class Framework
             $controller = $this->controllerResolver->getController($request);
             $arguments = $this->argumentResolver->getArguments($request, $controller);
 
-            return call_user_func_array($controller, $arguments);
+            $response = call_user_func_array($controller, $arguments);
         } catch (ResourceNotFoundException $exception) {
-            return new Response('Not Found', 404);
+            $response = new Response('Not Found', 404);
         } catch (\Exception $exception) {
-            return new Response('An error occurred', 500);
+            $response = new Response('An error occurred', 500);
         }
+
+        return $response;
     }
 }
